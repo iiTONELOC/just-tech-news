d9556702bf241c08e48729a177227703e54c9152
iiTONELOC <anthonytropeano@me.com>
2021-03-18 22:57:00 -0400

    added login

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const path = require('path');
 //IMPORT HANDLEBARS
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+//IMPORT SESSIONS
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 
 
 
@@ -17,6 +30,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
