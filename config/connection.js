732db536931a50c40d3acbe76e7b09aff9dd12e9
iiTONELOC <anthonytropeano@me.com>
2021-03-21 03:33:39 -0400

    did nothing

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        host: 'localhost',
         dialect: 'mysql',
         port: 3306
     });
